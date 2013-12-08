96934d7ab5cf357903ab5fbe6848fa5f131bbc86
Sir Anthony <anthony@adsorbtion.org>
2013-12-08 11:45:09 +0600

    glslang

--- GLSLCompiler/glslang/MachineIndependent/postProcess.sh
+++ GLSLCompiler/glslang/MachineIndependent/postProcess.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-perl -wne 'BEGIN { print "#include \"utils/dbgprint.h\""; } s/(fprintf|YYFPRINTF)\s*\([^,]*\s*/dbgPrint\(DBGLVL_COMPILERINFO/;print;' $1 > $2
+#dos2unix glslang.y
+perl -wne 'BEGIN { print "#include \"utils/dbgprint.h\"\n"; } s/(fprintf|YYFPRINTF)\s*\([^,]*\s*/dbgPrint\(DBGLVL_COMPILERINFO/;print;' $1.cpp > $2.cpp
+cp $1.hpp $2.hpp
+
