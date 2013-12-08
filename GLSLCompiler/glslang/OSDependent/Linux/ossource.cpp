0fe8080e7965866f41346112cd211d0cb4e267b1
Sir Anthony <anthony@adsorbtion.org>
2013-12-08 13:46:00 +0600

    Code style GLSLCompiler/glslang/OSDependent/Linux/

--- GLSLCompiler/glslang/OSDependent/Linux/ossource.cpp
+++ GLSLCompiler/glslang/OSDependent/Linux/ossource.cpp
@@ -42,7 +42,6 @@
 #error Trying to build a Linux specific file in a non-Linux build.
 #endif
 
-
 //
 // Thread cleanup
 //
@@ -53,9 +52,8 @@
 //
 void DetachThreadLinux(void *)
 {
-    DetachThread();
+	DetachThread();
 }
-
 
 //
 // Registers cleanup handler, sets cancel type and state, and excecutes the thread specific
@@ -65,77 +63,74 @@ void DetachThreadLinux(void *)
 //
 void OS_CleanupThreadData(void)
 {
-    int old_cancel_state, old_cancel_type;
-    void *cleanupArg = NULL;
+	int old_cancel_state, old_cancel_type;
+	void *cleanupArg = NULL;
 
-    //
-    // Set thread cancel state and push cleanup handler.
-    //
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &old_cancel_state);
-    pthread_cleanup_push(DetachThreadLinux, (void *) cleanupArg);
+	//
+	// Set thread cancel state and push cleanup handler.
+	//
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &old_cancel_state);
+	pthread_cleanup_push(DetachThreadLinux, (void *) cleanupArg);
 
-    //
-    // Put the thread in deferred cancellation mode.
-    //
-    pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, &old_cancel_type);
+			//
+			// Put the thread in deferred cancellation mode.
+			//
+			pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, &old_cancel_type);
 
-    //
-    // Pop cleanup handler and execute it prior to unregistering the cleanup handler.
-    //
-    pthread_cleanup_pop(1);
+			//
+			// Pop cleanup handler and execute it prior to unregistering the cleanup handler.
+			//
+			pthread_cleanup_pop(1);
 
-    //
-    // Restore the thread's previous cancellation mode.
-    //
-    pthread_setcanceltype(old_cancel_state, NULL);
+			//
+			// Restore the thread's previous cancellation mode.
+			//
+	pthread_setcanceltype(old_cancel_state, NULL);
 }
-
 
 //
 // Thread Local Storage Operations
 //
 OS_TLSIndex OS_AllocTLSIndex()
 {
-    pthread_key_t pPoolIndex;
+	pthread_key_t pPoolIndex;
 
-    //
-    // Create global pool key.
-    //
-    if ((pthread_key_create(&pPoolIndex, NULL)) != 0) {
-        assert(0 && "OS_AllocTLSIndex(): Unable to allocate Thread Local Storage");
-        return false;
-    }
-    else
-        return pPoolIndex;
+	//
+	// Create global pool key.
+	//
+	if ((pthread_key_create(&pPoolIndex, NULL)) != 0) {
+		assert(
+				0 && "OS_AllocTLSIndex(): Unable to allocate Thread Local Storage");
+		return false;
+	} else
+		return pPoolIndex;
 }
-
 
 bool OS_SetTLSValue(OS_TLSIndex nIndex, void *lpvValue)
 {
-    if (nIndex == OS_INVALID_TLS_INDEX) {
-        assert(0 && "OS_SetTLSValue(): Invalid TLS Index");
-        return false;
-    }
+	if (nIndex == OS_INVALID_TLS_INDEX) {
+		assert(0 && "OS_SetTLSValue(): Invalid TLS Index");
+		return false;
+	}
 
-    if (pthread_setspecific(nIndex, lpvValue) == 0)
-        return true;
-    else
-        return false;
+	if (pthread_setspecific(nIndex, lpvValue) == 0)
+		return true;
+	else
+		return false;
 }
-
 
 bool OS_FreeTLSIndex(OS_TLSIndex nIndex)
 {
-    if (nIndex == OS_INVALID_TLS_INDEX) {
-        assert(0 && "OS_SetTLSValue(): Invalid TLS Index");
-        return false;
-    }
+	if (nIndex == OS_INVALID_TLS_INDEX) {
+		assert(0 && "OS_SetTLSValue(): Invalid TLS Index");
+		return false;
+	}
 
-    //
-    // Delete the global pool key.
-    //
-    if (pthread_key_delete(nIndex) == 0)
-        return true;
-    else
-        return false;
+	//
+	// Delete the global pool key.
+	//
+	if (pthread_key_delete(nIndex) == 0)
+		return true;
+	else
+		return false;
 }
