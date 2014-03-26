d01ab37804ea46f5091349cc1d48fa9172fe9744
Sir Anthony <anthony@adsorbtion.org>
2014-03-26 22:03:15 +0600

    This is some bad thing I did, but it helps me to check generated shaders for syntax errors with glsl compiler.

---
+++ mesa-glsl/tests/generate_shader_checker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+OUTPUT_PATH=$1
+TEST_DIR=$2
+
+cat <<EOF
+#!/bin/sh
+SHADERS_PATH="${OUTPUT_PATH}/shaders"
+FILES_PATH="${TEST_DIR}/shaders"
+mkdir -p \${SHADERS_PATH}
+rm \${SHADERS_PATH}/*.{frag,geom}
+for type in geom frag; do
+  ( cd \$FILES_PATH/test.\${type}.dbgout
+    for file in \$(find . -type f ! -name rules); do
+      if grep --quiet "^no code$" \${file}; then continue; fi
+      sed '/^==/d' \${file} > \${SHADERS_PATH}/\${file}.\${type}
+    done )
+done
+EOF
